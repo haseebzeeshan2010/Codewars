952d3e3a0804c2c840ec48620a9183fc404ad33e
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-04-23 21:23:33 +0100

    Add best solution for the problem by 00kevn

--- Scramble_validator.py
+++ Scramble_validator.py
@@ -18,3 +18,12 @@ def scramble(s1, s2):
             return False
         s1.remove(i)
     return True
+
+#Best solution
+
+from collections import Counter
+def scramble(s1,s2):
+    # Counter basically creates a dictionary of counts and letters
+    # Using set subtraction, we know that if anything is left over,
+    # something exists in s2 that doesn't exist in s1
+    return len(Counter(s2)- Counter(s1)) == 0
