a3ba6c0d52222f103d3d476046878f9f068eda7f
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-04-25 16:11:23 +0100

    Add best solution possible by jolaf

--- find_observed_pin.py
+++ find_observed_pin.py
@@ -57,3 +57,11 @@ def get_pins(observed):
 
     return(result)
     
+#Best solution - by jolaf
+from itertools import product
+
+
+ADJACENTS = ('08', '124', '2135', '326', '4157', '52468', '6359', '748', '85790', '968')
+
+def get_pins(observed):
+    return [''.join(p) for p in product(*(ADJACENTS[int(d)] for d in observed))]
