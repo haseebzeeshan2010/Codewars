b5047b2851b8b96f3dafe1a2f265afd03ee00ffd
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-05-06 20:29:13 +0100

    Add task and best solution

--- strip_comments.py
+++ strip_comments.py
@@ -1,3 +1,25 @@
+# Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+
+# Example:
+
+# Given an input string of:
+
+# apples, pears # and bananas
+# grapes
+# bananas !apples
+# The output expected would be:
+
+# apples, pears
+# grapes
+# bananas
+# The code would be called like so:
+
+# result = strip_comments("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+# # result should == "apples, pears\ngrapes\nbananas"
+
+
+#MY CODE
+
 def strip(string, markers):
     found = False
     for j in range(0,len(string)):
@@ -33,3 +55,12 @@ def strip_comments(strng, markers):
             print("Aye")
             return new_string[1:]
     return new_string
+
+
+#BEST CODE by milesb
+
+def solution(string,markers):
+    parts = string.split('\n')
+    for s in markers:
+        parts = [v.split(s)[0].rstrip() for v in parts]
+    return '\n'.join(parts)
