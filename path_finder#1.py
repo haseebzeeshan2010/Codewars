150e6dbca519420cd604d47ef901b4849b4d5bec
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-05-14 16:45:56 +0100

    Add best solution

--- path_finder#1.py
+++ path_finder#1.py
@@ -68,3 +68,20 @@ def path_finder(maze):
     
     print(flood_fill(maze,0,0))
     return True # can go to lower right corner from upper left one
+
+
+
+
+#BEST SOLUTION
+
+def path_finder(maze):
+    matrix = list(map(list, maze.splitlines()))
+    stack, length = [[0, 0]], len(matrix)
+    while len(stack):
+      x, y = stack.pop()
+      if matrix[x][y] == '.':
+        matrix[x][y] = 'x'
+        for x, y in (x, y-1), (x, y+1), (x-1, y), (x+1, y):
+          if 0 <= x < length and 0 <= y < length:
+            stack.append((x, y))
+    return matrix[length-1][length-1] == 'x'
